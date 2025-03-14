c22e9d1041fc486ffebaea6c5f80795b657b5765
Michael Weinberger <michael.weinberger@nttdata.com>
2025-03-14 16:48:50 +0100

    add postgres and modify connection string

--- AllianzSampleXaf.Blazor.Server/Startup.cs
+++ AllianzSampleXaf.Blazor.Server/Startup.cs
@@ -1,18 +1,19 @@
-﻿using DevExpress.ExpressApp.Security;
+﻿using AllianzSampleXaf.Blazor.Server.Services;
 using DevExpress.ExpressApp.ApplicationBuilder;
 using DevExpress.ExpressApp.Blazor.ApplicationBuilder;
 using DevExpress.ExpressApp.Blazor.Services;
-using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components.Server.Circuits;
 using Microsoft.EntityFrameworkCore;
-using AllianzSampleXaf.Blazor.Server.Services;
-using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
 
 namespace AllianzSampleXaf.Blazor.Server;
 
-public class Startup {
-    public Startup(IConfiguration configuration) {
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
         Configuration = configuration;
     }
 
@@ -20,34 +21,40 @@ public class Startup {
 
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services) {
+    public void ConfigureServices(IServiceCollection services)
+    {
         services.AddSingleton(typeof(Microsoft.AspNetCore.SignalR.HubConnectionHandler<>), typeof(ProxyHubConnectionHandler<>));
 
         services.AddRazorPages();
         services.AddServerSideBlazor();
         services.AddHttpContextAccessor();
         services.AddScoped<CircuitHandler, CircuitHandlerProxy>();
-        services.AddXaf(Configuration, builder => {
+        services.AddXaf(Configuration, builder =>
+        {
             builder.UseApplication<AllianzSampleXafBlazorApplication>();
             builder.Modules
                 .AddConditionalAppearance()
-                .AddDashboards(options => {
+                .AddDashboards(options =>
+                {
                     options.DashboardDataType = typeof(DevExpress.Persistent.BaseImpl.EF.DashboardData);
                 })
-                .AddValidation(options => {
+                .AddValidation(options =>
+                {
                     options.AllowValidationDetailsAccess = false;
                 })
                 .Add<AllianzSampleXaf.Module.AllianzSampleXafModule>()
                 .Add<AllianzSampleXafBlazorModule>();
             builder.ObjectSpaceProviders
                 .AddSecuredEFCore(options => options.PreFetchReferenceProperties())
-                    .WithDbContext<AllianzSampleXaf.Module.BusinessObjects.AllianzSampleXafEFCoreDbContext>((serviceProvider, options) => {
+                    .WithDbContext<AllianzSampleXaf.Module.BusinessObjects.AllianzSampleXafEFCoreDbContext>((serviceProvider, options) =>
+                    {
                         // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
                         // Do not use this code in production environment to avoid data loss.
                         // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
                         //options.UseInMemoryDatabase("InMemory");
                         string connectionString = null;
-                        if(Configuration.GetConnectionString("ConnectionString") != null) {
+                        if (Configuration.GetConnectionString("ConnectionString") != null)
+                        {
                             connectionString = Configuration.GetConnectionString("ConnectionString");
                         }
 #if EASYTEST
@@ -56,14 +63,15 @@ public class Startup {
                         }
 #endif
                         ArgumentNullException.ThrowIfNull(connectionString);
-                        options.UseSqlServer(connectionString);
+                        options.UseNpgsql(connectionString);
                         options.UseChangeTrackingProxies();
                         options.UseObjectSpaceLinkProxies();
                         options.UseLazyLoadingProxies();
                     })
                 .AddNonPersistent();
             builder.Security
-                .UseIntegratedMode(options => {
+                .UseIntegratedMode(options =>
+                {
                     options.Lockout.Enabled = true;
 
                     options.RoleType = typeof(PermissionPolicyRole);
@@ -73,7 +81,8 @@ public class Startup {
                     // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
                     // If you use PermissionPolicyUser or a custom user type, comment out the following line:
                     options.UserLoginInfoType = typeof(AllianzSampleXaf.Module.BusinessObjects.ApplicationUserLoginInfo);
-                    options.Events.OnSecurityStrategyCreated += securityStrategy => {
+                    options.Events.OnSecurityStrategyCreated += securityStrategy =>
+                    {
                         // Use the 'PermissionsReloadMode.NoCache' option to load the most recent permissions from the database once
                         // for every DbContext instance when secured data is accessed through this instance for the first time.
                         // Use the 'PermissionsReloadMode.CacheOnFirstAccess' option to reduce the number of database queries.
@@ -83,24 +92,30 @@ public class Startup {
                         ((SecurityStrategy)securityStrategy).PermissionsReloadMode = PermissionsReloadMode.NoCache;
                     };
                 })
-                .AddPasswordAuthentication(options => {
+                .AddPasswordAuthentication(options =>
+                {
                     options.IsSupportChangePassword = true;
                 });
         });
-        var authentication = services.AddAuthentication(options => {
+        var authentication = services.AddAuthentication(options =>
+        {
             options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
         });
-        authentication.AddCookie(options => {
+        authentication.AddCookie(options =>
+        {
             options.LoginPath = "/LoginPage";
         });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-        if(env.IsDevelopment()) {
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
             app.UseDeveloperExceptionPage();
         }
-        else {
+        else
+        {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. To change this for production scenarios, see: https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
@@ -112,7 +127,8 @@ public class Startup {
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseXaf();
-        app.UseEndpoints(endpoints => {
+        app.UseEndpoints(endpoints =>
+        {
             endpoints.MapXafEndpoints();
             endpoints.MapBlazorHub();
             endpoints.MapFallbackToPage("/_Host");
